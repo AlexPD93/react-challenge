6fda5d8e4ab3e95a75f6e8ef9d3199a578deb519
oliverjam <oliverjamesphillips@gmail.com>
2022-10-13 18:41:25 +0100

    Render solutions on the page

--- solution/App.jsx
+++ solution/App.jsx
@@ -1,9 +1,13 @@
 import Greeting from "./Greeting.jsx";
+import Shouter from "./Shouter.jsx";
+import MouseTracker from "./MouseTracker.jsx";
 
 function App() {
   return (
     <main>
       <Greeting name="oli" />
+      <Shouter />
+      <MouseTracker />
     </main>
   );
 }
