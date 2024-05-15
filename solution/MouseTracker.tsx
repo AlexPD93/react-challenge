4bfc00f5886989d2d82eb53c20effd02e74fbe67
oliverjam <oliverjamesphillips@gmail.com>
2024-05-15 14:50:57 +0100

    An tuple type to MouseTracker solution
    This is a good example type

--- solution/MouseTracker.tsx
+++ solution/MouseTracker.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function MouseTracker() {
-	const [coords, setCoords] = useState([0, 0]);
+	const [coords, setCoords] = useState<[number, number]>([0, 0]);
 
 	useEffect(() => {
 		function update(e: MouseEvent) {
